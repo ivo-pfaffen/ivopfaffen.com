6397c604c6a06209221012a7351c61cb19750db9
Ivo <61206292+ivo-pfaffen@users.noreply.github.com>
2020-06-11 00:39:54 -0300

    ✅ Add project page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav__link");
+const navBack = document.querySelector(".nav-back");
+
 
 navToggle.addEventListener("click", () => {
     document.body.classList.toggle("nav-open");
@@ -10,3 +12,7 @@ navLinks.forEach(link => {
         document.body.classList.remove("nav-open");
     })
 }) 
+
+navBack.addEventListener("click", () => {
+    window.location.href = "../index.html";
+});
